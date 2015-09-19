7409939db2ef5e7a8d6eb7ae3fe6ea409af6c7d9
kidliaa <kidliaa@gmail.com>
2015-09-19 10:26:20 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('browser', function(){
+	browserSync.init(null, {
+		server: {
+			baseDir: './',
+			index: "./pikadate.html",
+		}
+	});
+})
+
+gulp.task('bs-reload', function(){
+	browserSync.reload();
+})
+
+gulp.task('watch',function(){
+	gulp.watch('./pikadate.html', ['bs-reload']);
+	gulp.watch('./css/**', ['bs-reload']);
+	gulp.watch('./js/**', ['bs-reload']);
+})
+
+
+
+gulp.task('default',['browser', 'watch'],function(){
+
+});
